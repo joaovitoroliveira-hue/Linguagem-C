4dbf679b5803d07b1fc3c226577d9bc7215a79c4
João Vitor de Oliveira Sobral <joaovitordeoliveirasobral@gmail.com>
2026-08-12 00:00:11 -0300

    Aula-01

---
+++ basico01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int idade;
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+    return(0);
+}
+/*
+
+  * int idade: Variável do tipo int e com nome idade.
+
+  * scanf: Permite escrever na tela.
+
+  * %d: Formatação para ler inteiro.
+
+  * &: Operador de endereço (indica onde vai ser armazenado algum valor).
+
+*/
---
+++ hello-world.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    printf("Hello World!\n");
+    return(0);
+}
+/*
+- Este é o código mais básico da Linguagem C, o famoso "Hello World". Em apenas 5 linhas de código reunimos muitas coisas
+  importantes por aqui.
+  * #include <stdio.h>: Diretrizes de pré-processador que importa bibliotecas de I/O padrão.
+  
+  * int main(): Função principal (ponto de partida obrigatório de qualquer programa em C).
+
+  * printf(): Função que exibe dados na tela. "\n" é um escape sequence que faz quebra de linha após a mensagem.
+
+  * return(0): 
+*/
