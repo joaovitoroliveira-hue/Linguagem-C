54900e37b70b4441f6c42b4fb3a3d554e25344ba
João Vitor de Oliveira Sobral <joaovitordeoliveirasobral@gmail.com>
2026-08-29 09:35:03 -0300

    Estruturas de Controle

--- Aula-03/condicionalSwitch.c
+++ Aula-03/condicionalSwitch.c
@@ -4,7 +4,7 @@ int main(){
     int dia = 3;
     
     switch (dia) {
-        case 1: printf("Sef\n"); break;
+        case 1: printf("Seg\n"); break;
         case 2: printf("Ter\n"); break;
         case 3: printf("Qua\n"); break;
         case 4: printf("Qui\n"); break;
